85557a849de72f0ab51f5a86eb4a0677d097c9c8
r03ert0 <rto@braincatalogue.org>
2020-03-27 14:48:31 +0100

    change auth provider to GitHub only

--- js/init-firebase.js
+++ js/init-firebase.js
@@ -9,10 +9,10 @@ const config = {
 const uiConfig = {
   signInSuccessUrl: '/', // <url-to-redirect-to-on-success>
   signInOptions: [
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+      /*
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      /*
-      firebase.auth.GithubAuthProvider.PROVIDER_ID
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.PhoneAuthProvider.PROVIDER_ID
